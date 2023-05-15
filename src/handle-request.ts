7b47b70cd5fdb3f0a306ff14d26aa626a90cae91
luckyporo <565010808@qq.com>
2023-05-15 13:12:26 +0800

    change to azure openai

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -1,48 +1,162 @@
-const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
-  const picked = new Headers();
-  for (const key of headers.keys()) {
-    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
-      const value = headers.get(key);
-      if (typeof value === "string") {
-        picked.set(key, value);
-      }
-    }
+// The name of your Azure OpenAI Resource.
+const resourceName=process.env.RESOURCE_NAME
+
+// The deployment name you chose when you deployed the model.
+const mapper = {
+    'gpt-3.5-turbo': process.env.DEPLOY_NAME_GPT35,
+    'gpt-4': process.env.DEPLOY_NAME_GPT4
+};
+
+const apiVersion="2023-03-15-preview"
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+export default async function handleRequest(request) {
+  if (request.method === 'OPTIONS') {
+    return handleOPTIONS(request)
   }
-  return picked;
-};
 
-const CORS_HEADERS: Record<string, string> = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "Content-Type, Authorization",
-};
+  const url = new URL(request.url);
+  if (url.pathname === '/v1/chat/completions') {
+    var path="chat/completions"
+  } else if (url.pathname === '/v1/completions') {
+    var path="completions"
+  } else if (url.pathname === '/v1/models') {
+    return handleModels(request)
+  } else {
+    return new Response('404 Not Found', { status: 404 })
+  }
 
-export default async function handleRequest(req: Request & { nextUrl?: URL }) {
-  if (req.method === "OPTIONS") {
-    return new Response(null, {
-      headers: CORS_HEADERS,
+  let body;
+  if (request.method === 'POST') {
+    body = await request.json();
+  }
+
+  const modelName = body?.model;  
+  const deployName = mapper[modelName] || '' 
+
+  if (deployName === '') {
+    return new Response('Missing model mapper', {
+        status: 403
+    });
+  }
+  const fetchAPI = `https://${resourceName}.openai.azure.com/openai/deployments/${deployName}/${path}?api-version=${apiVersion}`
+
+  const authKey = request.headers.get('Authorization');
+  if (!authKey) {
+    return new Response("Not allowed", {
+      status: 403
     });
   }
 
-  const { pathname, search } = req.nextUrl ? req.nextUrl : new URL(req.url);
-  const url = new URL(pathname + search, "https://api.openai.com").href;
-  const headers = pickHeaders(req.headers, ["content-type", "authorization"]);
-
-  const res = await fetch(url, {
-    body: req.body,
-    method: req.method,
-    headers,
-  });
-
-  const resHeaders = {
-    ...CORS_HEADERS,
-    ...Object.fromEntries(
-      pickHeaders(res.headers, ["content-type", /^x-ratelimit-/, /^openai-/])
-    ),
+  const payload = {
+    method: request.method,
+    headers: {
+      "Content-Type": "application/json",
+      "api-key": authKey.replace('Bearer ', ''),
+    },
+    body: typeof body === 'object' ? JSON.stringify(body) : '{}',
   };
 
-  return new Response(res.body, {
-    headers: resHeaders,
-    status: res.status
+  const response = await fetch(fetchAPI, payload);
+
+  if (body?.stream != true){
+    return response
+  } 
+
+  let { readable, writable } = new TransformStream()
+  stream(response.body, writable);
+  return new Response(readable, response);
+
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// support printer mode and add newline
+async function stream(readable, writable) {
+  const reader = readable.getReader();
+  const writer = writable.getWriter();
+
+  // const decoder = new TextDecoder();
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+// let decodedValue = decoder.decode(value);
+  const newline = "\n";
+  const delimiter = "\n\n"
+  const encodedNewline = encoder.encode(newline);
+
+  let buffer = "";
+  while (true) {
+    let { value, done } = await reader.read();
+    if (done) {
+      break;
+    }
+    buffer += decoder.decode(value, { stream: true }); // stream: true is important here,fix the bug of incomplete line
+    let lines = buffer.split(delimiter);
+
+    // Loop through all but the last line, which may be incomplete.
+    for (let i = 0; i < lines.length - 1; i++) {
+      await writer.write(encoder.encode(lines[i] + delimiter));
+      await sleep(30);
+    }
+
+    buffer = lines[lines.length - 1];
+  }
+
+  if (buffer) {
+    await writer.write(encoder.encode(buffer));
+  }
+  await writer.write(encodedNewline)
+  await writer.close();
+}
+
+async function handleModels(request) {
+  const data = {
+    "object": "list",
+    "data": []  
+  };
+
+  for (let key in mapper) {
+    data.data.push({
+      "id": key,
+      "object": "model",
+      "created": 1677610602,
+      "owned_by": "openai",
+      "permission": [{
+        "id": "modelperm-M56FXnG1AsIr3SXq8BYPvXJA",
+        "object": "model_permission",
+        "created": 1679602088,
+        "allow_create_engine": false,
+        "allow_sampling": true,
+        "allow_logprobs": true,
+        "allow_search_indices": false,
+        "allow_view": true,
+        "allow_fine_tuning": false,
+        "organization": "*",
+        "group": null,
+        "is_blocking": false
+      }],
+      "root": key,
+      "parent": null
+    });  
+  }
+
+  const json = JSON.stringify(data, null, 2);
+  return new Response(json, {
+    headers: { 'Content-Type': 'application/json' },
   });
+}
+
+async function handleOPTIONS(request) {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*'
+      }
+    })
 }
